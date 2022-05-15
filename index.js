fbfa5c6397988d39f68da0dc8e670306d397d768
Rafael Lotto Machado <ralomach@gmail.com>
2022-05-15 17:28:23 -0300

    Project finished

--- index.js
+++ index.js
@@ -1,24 +1,62 @@
 require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const app = express();
 
-// Basic Configuration
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dns = require('dns');
+const express = require('express');
+const validUrl = require('valid-url');
+
+const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use('/public', express.static(__dirname + '/public'));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get('/', (_, res) => {
+  res.sendFile(__dirname + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+let urls = new Set();
+
+app.post('/api/shorturl', (req, res) => {
+  const errorMessage = { error: 'invalid url' };
+
+  // Get url from input
+  let url = new URL(req.body.url);
+
+  // Test the url's domain
+  dns.lookup(url.hostname, (err) => {
+    // If the url is not valid, show error message
+    if (
+      !validUrl.isUri(url.origin) ||
+      (err && err.code === 'ENOTFOUND')
+    ) {
+      return res.json(errorMessage);
+    }
+
+    // Url is valid, so add it to the set
+    urls.add(req.body.url);
+
+    // Show success message
+    res.json({
+      "original_url": req.body.url,
+      "short_url": [...urls].indexOf(req.body.url) + 1,
+    });
+  });
 });
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
+app.get('/api/shorturl/:id', (req, res) => {
+  let url = [...urls][req.params.id - 1];
+
+  if (url === undefined) {
+    return res.json({ "error": "Short url not found" });
+  }
+
+  res.redirect(url);
+});
+
+app.listen(port, function () {
+  console.log(`Server running at http://localhost:${port}`);
 });
